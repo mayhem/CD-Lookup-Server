b77eef4354c1a7be710a3d80a73e9ae8029eb777
Robert Kaye <robert@rump.(none)>
2009-07-28 20:14:04 -0700

    Basic functions are almost working, but I need a better server to test with

--- lib/index.py
+++ lib/index.py
@@ -3,18 +3,25 @@
 import psycopg2
 import numpy as np
 import scikits.ann as ann
+import time
+from math import sqrt
 
-
-IDS_PER_CHUNK            = 2000
+IDS_PER_CHUNK            = 20 #00
 KDTREE_DIM               = 6 
 NUM_TRACKS_PER_TRACKLIST = KDTREE_DIM - 1
 NUM_TRACKS_SCALE_FACTOR  = 10000
 SECTORS_PER_SECOND       = 75
+MAX_NUM_TRACKS           = 99
+MIN_NUMBER_OF_TRACKS     = 4
 
-class CDLookup(object):
+class TreeNotLoaded(Exception):
+    pass
+
+class CDLookupIndex(object):
     '''
     Load the track durations from the database and create a ANN kdtree in order to
-    find nearest neighbors in "CD Table of contents" space 
+    find nearest neighbors in "CD Table of contents" space. This
+    class is only a proof-of-concept -- it not multi-thread or multi-process safe yet.
     '''
 
     def __init__(self, host, database, user, passwd):
@@ -23,19 +30,26 @@ class CDLookup(object):
         self.user = user
         self.passwd = passwd
 
+        # Load the kdtree into memory
+        self.kdtree, self.tracklists = self.load_data()
+
     def validate_toc(self, toc):
-        '''Ensure that the passed toc is valid'''
+        '''Ensure that the passed toc is valid. Returns True or False'''
 
-        first, num_tracks, leadout, offsets = toc.split(' ', 3)
-        num_tracks = int(num_tracks)
-        if int(first) != 1: return False
-        if num_tracks < 1 or num_tracks > MAX_NUM_TRACKS: return False
+        try:
+            first, num_tracks, leadout, offsets = toc.split(' ', 3)
+            leadout = int(leadout)
+            num_tracks = int(num_tracks)
+            if int(first) != 1: return False
+            if num_tracks < 1 or num_tracks > MAX_NUM_TRACKS: return False
 
-        offsets = offsets.split()
-        for offset in offsets:
-            if offset >= leadout: return False
+            offsets = offsets.split()
+            for offset in offsets:
+                if int(offset) >= leadout: return False
 
-        if len(offsets) != num_tracks: return False
+            if len(offsets) != num_tracks: return False
+        except ValueError:
+            return False
 
         return True
 
@@ -49,10 +63,8 @@ class CDLookup(object):
 
         durations = []
         for i in xrange(len(offsets) - 1):
-            durations.append((offsets[i + 1] - offsets[i]) * SECTORS_PER_SECOND)
+            durations.append((int(offsets[i + 1]) - int(offsets[i])) * 1000 / SECTORS_PER_SECOND)
 
-        print toc
-        print durations
         return durations
 
     def select_tracks(self, tracks):
@@ -60,7 +72,6 @@ class CDLookup(object):
         Deterministically pick tracks from an array of tracks. Since tracklists with
         more than NUM_TRACKS_PER_TRACKLIST can't all fit into the kdTree, we have to
         pick at max NUM_TRACKS_PER_TRACKLIST tracks.
-        Warning: The argument tracks may have zeros appended to it to fit the point size
         '''
 
         # The first dimension will be the number of tracks on a given tracklist  
@@ -87,12 +98,27 @@ class CDLookup(object):
 
         return point
 
-    def lookup(self, kdtree, toc):
+    def lookup(self, toc, thresholdDistance):
         '''Carry out a lookup of a toc'''
-        point = self.select_tracks(self.convert_toc_to_durations(toc))
-        ret = k.knn(point, 1)
 
-    def load_data_from_db(self):
+        if not self.kdtree: raise TreeNotLoaded()
+
+        point = self.select_tracks(self.convert_toc_to_durations(toc))
+        ret = self.kdtree.knn(point, 10)
+        print ret
+
+        print len(self.tracklists)
+        r2 = thresholdDistance * thresholdDistance
+        out = []
+        for p, dist in zip(ret[0][0], ret[1][0]):
+            print "%d -> %d" % (p, self.tracklists[p])
+            print dist
+            print
+            if dist < r2:
+                out.append("{%d, %f}" % (self.tracklists[p], sqrt(dist)))
+        return "[" + ",".join(out) + "]\n"
+
+    def load_data(self):
         '''
         Create an ANN kdtree from the track durations in the DB
         '''
@@ -119,8 +145,10 @@ class CDLookup(object):
         numChunks = (totalRows / IDS_PER_CHUNK) + 1
 
         # For debugging
-        numChunks = 10
+        numChunks = 1
+        tracklistIndexes = []
         for i in xrange(numChunks):
+            # TODO: Filter out tracklists that have fewere than MIN_NUMBER_OF_TRACKS tracks
             curs.execute("""SELECT t.tracklist, r.length 
                               FROM musicbrainz.track t, musicbrainz.recording r 
                              WHERE t.recording = r.id 
@@ -135,9 +163,10 @@ class CDLookup(object):
             invalid = False
             for row in rows:
                 if curTracklist > 0 and row[0] != curTracklist:
-                    if not invalid:
+                    if not invalid and len(durations) >= MIN_NUMBER_OF_TRACKS:
                         point = self.select_tracks(durations)
                         np.put(points[rowsProcessed], xrange(0, KDTREE_DIM), point)
+                        tracklistIndexes.append(row[0])
                         rowsProcessed += 1
                     durations = []
                     invalid = False
@@ -146,18 +175,13 @@ class CDLookup(object):
                 durations.append(row[1])
                 curTracklist = row[0]
 
-            print "%d of %d (%d%% done)" % (rowsProcessed, totalRows, int(100 * rowsProcessed / totalRows))
+            if i > 0 and i % 5000 == 0:
+                print "%d%% loaded" % int(100 * rowsProcessed / totalRows)
 
         conn.close()
 
         print "processed %d rows." % rowsProcessed
         print "build tree"
-
-        return ann.kdtree(points)
-
-cd = CDLookup("localhost", "musicbrainz_db_ngs", "musicbrainz_user", "")
-k = cd.load_data_from_db()
-
-print k.knn([90000, 318970, 261543, 364316, 254706, 397226], 1)
-print k.knn([120000, 150906, 214733, 192960, 232266, 170000], 1)
-
+        tree = ann.kdtree(points)
+        print "init done"
+        return tree, tracklistIndexes
--- lib/server.py
+++ lib/server.py
@@ -1,50 +1,50 @@
 #!/usr/bin/env python
 
-from wsgiref.simple_server import make_server
 import cgi
 import sys
 from cgi import FieldStorage;
+from werkzeug import run_simple
+from index import CDLookupIndex
 
-MAX_NUM_TRACKS   = 99
-DEFAULT_DISTANCE = 0.0
+DISTANCE_THRESHOLD = 10000
 
+cdlookup_index = None
 
 def bad_request(start_response, msg):
     '''Return a bad request response to the caller'''
     start_response('403 BAD REQUEST', [('Content-Type', 'text/plain')])
     return msg 
 
-def lookup(start_response, toc, distance):
-    '''Carry out a lookup'''
-
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ['[]\n']
-
 def cdlookup_server(environ, start_response):
     """WSGI server application for cdlookup server. This handler validates requests and carries out lookups"""
+
+    global cdlookup_index
 
     if environ['REQUEST_METHOD'].upper() != 'GET':
         return bad_request(start_response, "Only GET method accepted\n")
 
     url = environ["PATH_INFO"]
     if not url.startswith("/ws/1/toc"): 
-        return bad_request(start_response, "Invalid URL requested. Only /ws/1/toc/<toc> is supported.")
+        return bad_request(start_response, "Invalid URL requested. Only /ws/1/toc/<toc> is supported.\n")
     toc = url[10:].replace("+", " ")
 
     args = FieldStorage(environ=environ)
-    if not validate_toc(toc):
-        return bad_request(start_response, "Invalid toc passed.")
+    if not cdlookup_index.validate_toc(toc):
+        return bad_request(start_response, "Invalid toc passed.\n")
 
     try:
-        distance = args['distance']
+        distance = int(args.getvalue('dist', 0))
+        print distance
         if distance < 0.0:
-            return bad_request(start_response, "The distance parameter must be a positive float value.")
+            return bad_request(start_response, "The distance parameter must be a positive int value.\n")
     except KeyError:
-        distance = DEFAULT_DISTANCE
+        distance = DISTANCE_THRESHOLD
 
-    return lookup(start_response, toc, distance)
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return cdlookup_index.lookup(toc, distance)
+
+# create the lookup index
+cdlookup_index = CDLookupIndex("localhost", "musicbrainz_db_ngs", "musicbrainz_user", "")
 
 # Run a development server
-httpd = make_server('localhost', 8000, cdlookup_server);
-while(True):
-    httpd.handle_request()
+run_simple('localhost', 8000, cdlookup_server, use_reloader=True, threaded=True, processes = 0);
